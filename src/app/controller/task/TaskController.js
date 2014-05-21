333507dbd9d02bc3f61bb232397ac28c7dc02f7c
Deniss Alimovs <deniss.alimovs@gmail.com>
2014-05-21 13:30:49 +1000

    fix typo

--- src/app/controller/task/TaskController.js
+++ src/app/controller/task/TaskController.js
@@ -4,7 +4,7 @@ App.TaskController = Em.ArrayController.extend({
 	* Determines if a task is for an existing project.
 	*
 	* If the creation date of the task is after the project's due date
-	* this means the task if for an existing project.
+	* this means the task is for an existing project.
 	*/
 	isForExistingProject: function() {
 		return this.get('created').isAfter(
